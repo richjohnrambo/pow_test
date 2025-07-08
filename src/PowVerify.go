b1c59a5a3cc961e53e84b2fac8ea044e1e19b77c
richjohnrambo <richjohnrambo@gmail.com>
2025-07-08 17:26:32 +0800

    pow 模拟

---
+++ src/PowVerify.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+/**题目#1
+实践 POW， 编写程序（编程语言不限）用自己的昵称 + nonce，不断修改nonce 进行 sha256 Hash 运算：
+
+直到满足 4 个 0 开头的哈希值，打印出花费的时间、Hash 的内容及Hash值。
+再次运算直到满足 5 个 0 开头的哈希值，打印出花费的时间、Hash 的内容及Hash值。
+提交程序你的 Github 链接*/
+
+func main() {
+	name := "rambo"
+	begin := time.Now().UnixMilli()
+	for {
+		nonce := rand.Int()
+		data := []byte(name + strconv.Itoa(nonce))
+		hash := sha256.Sum256(data)
+		var shastr1 = fmt.Sprintf("%x", hash)
+		if shastr1[0:1] == "0000" {
+			fmt.Println(shastr1)
+			end := time.Now().UnixMilli()
+			fmt.Printf("time cost, %d\n", end-begin)
+			break
+		}
+	}
+
+}
